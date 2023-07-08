a02f8af748269aac6853711f5f37f9678b55fb24
ingjesusd <ing.jesusdiaz.r@gmail.com>
2023-07-08 00:52:23 -0500

    core 1

---
+++ sql/SQLQuery1crudcore.sql
@@ -0,0 +1,60 @@
+create database DBCRUDCORE
+
+USE DBCRUDCORE
+
+CREATE TABLE CONTACTO(
+Idcontacto int identity primary key, 
+Nombre varchar (50),
+Telefono varchar (50),
+correo varchar (50)
+)
+
+ 
+ create procedure sp_Listar
+ as
+ begin
+ select * from CONTACTO
+ end
+
+ create procedure sp_Obtener(
+ @IdContacto int
+ )
+ as
+ begin
+ select * from CONTACTO where  IdContacto = @IdContacto 
+ end
+
+
+create procedure sp_Guardar(
+@Nombre varchar (100),
+@Telefono varchar (100),
+@correo varchar (100)
+ )
+ as
+ begin
+ insert into CONTACTO(Nombre, Telefono, Correo) values (@Nombre, @Telefono @Correo)
+ end
+
+
+create procedure sp_Editar(
+@IdContacto int,
+@Nombre varchar (100),
+@Telefono varchar (100),
+@correo varchar (100)
+)
+ as
+ begin
+	update CONTACTO set Nombre = @Nombre, Telefono = @Telefono, Correo =@Correo Where IdContacto = @IdContacto
+end
+
+
+
+
+create procedure sp_Eliminar(
+@IdContacto int
+)
+as
+ begin
+
+ delete from CONTACTO where IdContacto = @IdContacto
+ end
